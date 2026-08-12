4172079f96976178ca38742220bf2176a082ab70
chas <chas@noreply.localhost>
2026-08-12 21:20:45 +0100

    cache last scaled image using dimage_scale: increase FPS by around 10

--- src/gint-extras/display.c
+++ src/gint-extras/display.c
@@ -4,14 +4,24 @@
 
 typedef struct image_linear_map image_linear_map;
 
-void dimage_scale(int x, int y, float scale, const bopti_image_t *image) {
-	image_linear_map map;
-	image_scale(image, scale*65536, scale*65536, &map);
+const bopti_image_t *last_image;
+bopti_image_t *scaled_image;
 
-	bopti_image_t *scaled_image = image_linear_alloc(image, &map);
+void dimage_scale(int x, int y, float scale, const bopti_image_t *image) {
+	if (last_image != image) {
+		if (image_valid(scaled_image)) {
+			image_free(scaled_image);
+		}
+
+		image_linear_map map;
+		image_scale(image, scale*65536, scale*65536, &map);
+
+		scaled_image = image_linear_alloc(image, &map);
+		last_image = image;
+	}
+
 	dimage(x, y, scaled_image);
 
-	image_free(scaled_image);
 }
 
 void dsubimage_scale(int x, int y, const bopti_image_t *image, int left, int top, int width, int height, float scale) {
